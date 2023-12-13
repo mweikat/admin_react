ab4d2f999f1231cd986b457e8a07a2f7f1d3321d
Jose Ramirez DeFranceschi <EJRD1102@Itauchile.cl>
2023-12-13 18:38:41 -0300

    add

--- src/commons/formsComponents/inputPasswordLogin.tsx
+++ src/commons/formsComponents/inputPasswordLogin.tsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { inputProps } from "./InterfaceInput";
+import { inputProps } from "./interfaceInput";
 
 export const InputPaswordLogin = (props:inputProps) => {
 
